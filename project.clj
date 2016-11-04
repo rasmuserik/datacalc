8e623fe046654209dfd0a993f5cd7edd17a8f260
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-11-04 18:35:17 +0100

    only simple optimisations, to allow reflection in app

--- project.clj
+++ project.clj
@@ -56,7 +56,7 @@
      {:output-to "index.js"
       :main solsort.touchlang.touchlang
       :externs ["util/externs.js"]
-      :optimizations :advanced
+      :optimizations :simple
       :pretty-print false}}
     ]}
 )
