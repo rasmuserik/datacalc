746294cd6805b62522843d72988f6c97d81564c6
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-11-14 23:47:00 +0100

    translate source into objs

--- scratch/parse.js
+++ scratch/parse.js
@@ -1,51 +1,162 @@
 var s = require('fs').readFileSync("code.ul", "utf-8").split('');
 
-// NB: written in a style easily translateable into touchlang, not nice JS-style
+// NB: written in a style easily translateable into touchlang, 
+// not nice JS-style, and will overflow stack on large data
 
-function arrpush(arr, x) {
-  return arr.concat([x]);
+function List(car, cdr) {
+  this.car = car;
+  this.cdr = cdr;
+};
+List.prototype.first = function() {
+  return this.car;
+};
+List.prototype.rest = function() {
+  return this.cdr;
+};
+List.prototype.reverse = function() {
+  var result = null;
+  var cell = this;
+  while(cell !== null) {
+    result = cons(cell.car, result);
+    cell = cell.cdr;
+  }
+  return result;
+};
+List.prototype.toArray = function() {
+  var result = [];
+  var cell = this;
+  while(cell) {
+    result.push(cell.car);
+    cell = cell.cdr;
+  }
+  return result
+};
+List.prototype.toJSON = function() {
+  return this.toArray().map(function(o) {
+      return (o && o.toJSON) ? o.toJSON() : o;
+  });
+};
+function cons(car, cdr) {
+  return new List(car, cdr);
 }
-function tokens(s, pos, tok) {
-  var c = s[pos];
-  if(pos >= s.length) {
-    return tok;
+List.fromArray = function(arr) {
+  var i = arr.length;
+  var result = null;
+  while(i) {
+    --i;
+    result = cons(arr[i], result);
+  }
+  return result;
+};
+
+function tokenize(chars, tokens) {
+  var c = chars && chars.first();
+  if(c === null) {
+    return tokens.reverse();
   } else if(c === ' ' || c === '\n') {
-    return tokens(s, pos + 1, tok);
+    return tokenize(chars.rest(), tokens);
   } else if(c == ')' || c == '(') {
-    return tokens(s, pos + 1, arrpush(tok, c));
+    return tokenize(chars.rest(), cons(c,  tokens));
   } else if(c == '"') {
-    pos = pos + 1;
-    c = s[pos];
-    var res = "\"";
-    while(pos <= s.length && c !== '"') {
+    var result = c;
+    c = "";
+    while(chars !== null && c !== '"') {
+      chars = chars.rest();
+      c = chars && chars.first();
       if(c === "\\") {
-        pos = pos + 1;
-        c = s[pos];
-        res += c;
+        chars = chars.rest();
+        c = chars && chars.first();
+        result += c;
       } else {
-        res += c;
+        result += c;
       }
-      pos = pos + 1;
-      c = s[pos];
     }
-    res += c;
-    return tokens(s, pos + 1, arrpush(tok, res))
+    return tokenize(chars.rest(), cons(result, tokens))
   } else if('0' <= c && c <= '9') {
-    var res = c;
-    while(pos <= s.length && '0' <= c && c <= '9') {
-      res += c;
-      pos = pos + 1;
-      c = s[pos];
+    var result = '';
+    while(chars !== null && '0' <= c && c <= '9') {
+      result += c;
+      chars = chars.rest();
+      c = chars && chars.first();
     }
-    return tokens(s, pos, arrpush(tok, parseInt(res, 10)))
+    return tokenize(chars, cons(parseInt(result, 10), tokens))
   } else {
-    var res = c;
-    while(pos <= s.length && c !== ')' && c !== '(' && c !== " " && c !== "\n") {
-      res += c;
-      pos = pos + 1;
-      c = s[pos];
+    var result = '';
+    while(chars !== null && "() \n".indexOf(c) === -1) {
+      result += c;
+      chars = chars.rest();
+      c = chars && chars.first();
     }
-    return tokens(s, pos, arrpush(tok, res));
+    return tokenize(chars, cons(result, tokens));
   }
 }
-console.log(tokens(s, 0, []));
+
+function reverse(list) {
+  return list && list.reverse();
+}
+function makeTree(tokens, stack, acc) {
+  if(tokens === null) {
+    return acc.reverse();
+  } else {
+    var token = tokens.first();
+    var tokens = tokens.rest();
+    if(token === "(") {
+      return makeTree(tokens, cons(acc, stack), null);
+    } else if(token === ")") {
+      return makeTree(tokens, stack.rest(), cons(acc.reverse(), stack.first()));
+    } else {
+      return makeTree(tokens, stack, cons(token, acc));
+    }
+  }
+}
+
+function parse(name, expr, i, acc) {
+  if(name && name !== "_") { 
+    name = name;
+  } else {
+    name = "$" + i;
+    i = i + 1
+  }
+  if(expr.constructor === List) {
+    var fn = expr.first();
+    var list = expr.rest();
+    var args = null;
+    while(list !== null) {
+      expr = list.first();
+      list = list.rest();
+      i = i + 1;
+      var id = "$" + i;
+      if(typeof expr === "string" && expr[0] !== '"') {
+        args = cons(expr, args);
+      } else {
+        var pair = parse(id, expr, i, acc);
+        i = pair.car;
+        acc = pair.cdr;
+        args = cons(id, args);
+      }
+    }
+    return cons(i, cons({id: name, fn: fn, args: reverse(args)}, acc));
+  } else {
+    return cons(i+1, cons({id: name, data: expr}, acc));
+  }
+}
+function parsePairs(list, i, acc) {
+  if(list === null) {
+    return acc;
+  } else {
+    var name = list.first();
+    list = list.rest();
+    var pair = parse(name, list.first(), i, acc);
+    i = pair.car;
+    acc = pair.cdr;
+    list = list.rest();
+    return parsePairs(list, i, acc);
+  }
+}
+s =  List.fromArray(s);
+tokens = tokenize(s, null)
+console.log(JSON.stringify(tokens));
+tree = makeTree(tokens, null, null);
+console.log(JSON.stringify(tree));
+console.log(JSON.stringify(parsePairs(tree, 0, null)));
+    
